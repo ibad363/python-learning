77d5daf320805022a4a5a7bb506d41c3e704e833
ibad363 <hafizibadurrehman363@gmail.com>
2025-03-14 17:18:06 +0500

    practice string methods

--- Practice/basic-practice/app.py
+++ Practice/basic-practice/app.py
@@ -37,14 +37,27 @@ ut labore et dolore magna aliqua.'''
 # one line string
 text_double = "Hell'o, Python"
 text_double = "Hell""o, Python"
-text_double = "Hello," \
-" Python"
+text_double = "Hell" \
+"o, Python"
 # print(text_double)
 
+# Accessing string characters
+# print("Character at index 1:", text_double[1]) # Output: e
+
+
+text = "Hell", "o Python"
+# print(text) # now it's tuple
 
 # list
 my_list_1: int = [1, 2, 3, "Java", 3.14, True]
 # print(my_list_1[3][1])
+
+# tuple
+my_tuple = (1, "AI", 2.71, [1, 2, 3])
+# print("\nTuple Example:", my_tuple)
+# Modifying a list inside a tuple
+my_tuple[3].append(4)
+# print("Modified Tuple with List:", my_tuple)  # The list inside the tuple is mutable
 
 # for i in range(1,6,2):
 #     print(i) #1,3,5
@@ -58,4 +71,37 @@ my_list_1: int = [1, 2, 3, "Java", 3.14, True]
 my_set = {1, 4, 111, "Ibad"}
 my_set: frozenset = frozenset({1, 4, 111, "Ibad",False,True,2233,3,"3"})
 my_set: frozenset = frozenset([1, 4, 111, "Ibad"])
-print(type(my_set), "my_set = ", my_set)
+# print(type(my_set), "my_set = ", my_set)
+
+
+# string methods
+string = "iBad ur rehman"
+# string_method = string.casefold()
+# string_method = string.capitalize()
+# string_method = string.center(30,"i")
+# string_method = string.center(30,)
+# string_method = string.count("a")
+# string_method = string.encode()
+# print(string_method)
+# print(type(string_method))
+# string_method = string.endswith("n")
+# string_method = string.find("ur")
+# txt = "For only {price:.2f} dollars!"
+# print(txt.format(price=56.2222))
+# string_method = string.index("z") # it shows error 
+string2 = "python developer "
+# string_method = string2.split("e") # ['python d', 'v', 'lop', 'r']
+# join_string = "e".join(string_method) # python developer
+# string_method = "e" in string2
+# string_method = "a" not in string2
+# string_method = string2.__len__()
+# string_method = string2 + " " + "hun"
+# string_method = string2[0:6] # slicing, last param is exclusive, output: python
+# string_method = string2.upper()
+# string_method = string2.lower()
+# string_method = string2.replace("python","typescript")
+string_method = string2 * 5
+print(string_method)
+
+
+# list methods
