17a7fa6bcf9733d1e9d9e0e83c23e682d2aea7ef
Ibad Ur Rehman <HAFIZIBADURREHMAN363@GMAIL.COM>
2025-03-19 16:24:37 +0500

    class 4 code

---
+++ Practice/operators,type-casting/app.py
@@ -0,0 +1,21 @@
+# type casting
+my_dic = {"name":"Ibad","age":20}
+print(str(my_dic))
+# print(type(int(my_dic)))
+
+i = 10
+# print(str(type(i)).upper())
+print(type(str(type(i))))
+print(type(bool(type(i))))
+
+
+b = None
+print(bool(i))
+
+if i:
+    print("Ibad")
+else:
+    print("kya hogaya")
+
+
+# operators, operand
